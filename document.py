383a071c9b27c7c6744d90e9bfa91d807a87542b
joyrav09 <56099222+joyrav09@users.noreply.github.com>
2021-05-25 00:43:47 +0700

    Add files via upload

--- document.py
+++ document.py
@@ -3,70 +3,116 @@ from tkinter import ttk
 from tkinter import filedialog
 import tkinter.messagebox as mb
 import pymysql
+import tkinter.ttk as ttk 
+import os
 
-class document():
-    def __init__(self, root):
+
+class MyApp(Tk):
+    def __init__(self):
+        Tk.__init__(self)
+        container = ttk.Frame(self)
+        container.pack(side="top", fill="both", expand=True)
+        self.frames = {}
+        for F in (MenuUtama, program):
+            frame = F(container, self)
+            self.frames[F] = frame
+            frame.grid(row=0, column=0, sticky='NSEW')
+        self.show_frame(MenuUtama)
+
+    def show_frame(self, cont):
+        frame = self.frames[cont]
+        frame.tkraise()
+
+
+
+class MenuUtama(ttk.Frame):
+    def __init__(self, root, controller):
+        self.controller = controller
+        ttk.Frame.__init__(self, root)
+        self.make_widget()
+
+    def make_widget(self):
+        self.cvs = Canvas(self, width="1020", height="540", background="#D9D9D9")
+       
+
+        # demo button to change page
+        namaLabel = Label(self.cvs, text="SELAMAT DATANG", font=("Cambria", 30, "bold"), background="#D9D9D9")
+        namaLabel.place(x=345, y=190)
+        btnMasuk = Button(self.cvs, text="Masuk", command=lambda: self.controller.show_frame(program), 
+                            bg="#A1A5A6", font=("Cambria", 12, "bold"))
+        btnMasuk.place(x=460, y=250, width="100", height="35")
+        self.cvs.pack()
+
+class document(ttk.Frame):
+    def __init__(self, root, controller):
         self.root = root
-        self.root.title("Document Management System")
-        self.root.geometry("1020x540")
-        self.root.resizable(False, False)
+        self.controller = controller   
+        ttk.Frame.__init__(self, root)
+        self.make_widget()
 
-
+    def make_widget(self):
         self.var_name = StringVar()
         self.var_nim = StringVar()
         self.var_prodi = StringVar()
 
         self.var_search = StringVar()
-        
-        #Tampilan Utama
-        searchbtn = Button(text="Cari", command=self.search_data)
+
+        #TAMPILAN UTAMA
+        searchbtn = Button(self, text="Cari", command=self.search_data, bg="#A1A5A6")
         searchbtn.place(x=849,y=15)
-        showallhbtn = Button(text="Semua", command=self.show_data)
+        showallhbtn = Button(self, text="Semua", command=self.show_data, bg="#A1A5A6")
         showallhbtn.place(x=890,y=15)
         
-        search = Entry(textvariable=self.var_search, bd=3, relief=GROOVE, width=30)
+        search = Entry(self, textvariable=self.var_search, bd=3, relief=GROOVE, width=30)
         search.place(x=650,y=14, height=27)
         
-        downloadbtn = Button(text="Download", command=self.download)
+        downloadbtn = Button(self, text="Download", command=self.download, bg="#A1A5A6")
         downloadbtn.place(x=550, y=15)
-        deletebtn = Button(text="Hapus", command=self.delete_data)
+        deletebtn = Button(self, text="Hapus", command=self.delete_data, bg="#A1A5A6")
         deletebtn.place(x=490,y=15)
+
+        btnkembali = Button(self, text='Kembali',
+                             command=lambda: self.controller.show_frame(MenuUtama), bg="#A1A5A6")
+        btnkembali.place(x=950, y=14.5)
         #===================================
 
-        #set tombol Keluar
+        #SET TOMBOL KELUAR
         #self.imgKeluar = PhotoImage(file='logout.png')
         #self.btnKeluar = Button(image=self.imgKeluar, compound='top', command=self.close)
         #self.btnKeluar.place(x=970, y=12.5, height=30)
         #===================================
 
-        #Tabel 1 
-        table1 = Frame(self.root, bd=4, relief=RIDGE, bg="green")
+        #TABEL 1
+        table1 = Frame(self, bd=4, relief=RIDGE, bg="#D9D9D9")
         table1.place(y=50, width=290, height=450)
 
-        name_document= Label(table1, text="DOKUMEN", bg="green", font=("cambria", 20, "bold"))
+        name_document= Label(table1, text="DOKUMEN", bg="#D9D9D9", font=("cambria", 20, "bold"))
         name_document.grid(row=0, columnspan=2, pady=5, padx=75)
 
-        #Label
-        label_name = Label(table1, text="Nama ", bg="green", font=("cambria", 12))
+        #LABEL 1
+        label_name = Label(table1, text="Nama ", bg="#D9D9D9", font=("cambria", 12))
         label_name.grid(row=1,column=0, pady=10, padx=10, sticky="w")
-        label_nim = Label(table1, text="NIM ", bg="green", font=("cambria", 12))
+        label_nim = Label(table1, text="NIM ", bg="#D9D9D9", font=("cambria", 12))
         label_nim.grid(row=2,column=0, pady=10, padx=10, sticky="w")
-        label_prodi = Label(table1, text="Prodi", bg="green", font=("cambria", 12))
+        label_prodi = Label(table1, text="Prodi", bg="#D9D9D9", font=("cambria", 12))
         label_prodi.grid(row=3,column=0, pady=10, padx=10, sticky="w")
 
-        entry_name = Entry(table1,textvariable=self.var_name, bd=3, relief=GROOVE, width=30)
-        entry_name.grid(row=1, column=1, pady=10, padx=10, sticky="w")
-        entry_nim = Entry(table1,textvariable=self.var_nim, bd=3, relief=GROOVE, width=30)
-        entry_nim.grid(row=2, column=1, pady=10, padx=10, sticky="w")
-        entry_prodi = Entry(table1,textvariable=self.var_prodi, bd=3, relief=GROOVE, width=30)
-        entry_prodi.grid(row=3, column=1, pady=10, padx=10, sticky="w") 
+        self.entry_name = Entry(table1,textvariable=self.var_name, bd=3, relief=GROOVE, width=30)
+        self.entry_name.grid(row=1, column=1, pady=10, padx=10, sticky="w")
+        self.entry_nim = Entry(table1,textvariable=self.var_nim, bd=3, relief=GROOVE, width=30)
+        self.entry_nim.grid(row=2, column=1, pady=10, padx=10, sticky="w")
+        self.entry_prodi = Entry(table1,textvariable=self.var_prodi, bd=3, relief=GROOVE, width=30)
+        self.entry_prodi.grid(row=3, column=1, pady=10, padx=10, sticky="w")
+
+        self.label = Label(table1, text = "", bg="#D9D9D9")
+        self.label.place(x =10, y=300) 
         #========================================
 
-        #Tabel 2
-        table2 = Frame(self.root, bd=4, relief=RIDGE, bg="white")
+        #TABEL 2
+        table2 = Frame(self, bd=4, relief=RIDGE, bg="white")
         table2.place(y=50, x=300, width=719, height=450)
         
-        #scroll
+        #SCROLL
         scroll_x= Scrollbar(table2, orient=HORIZONTAL)
         scroll_y= Scrollbar(table2, orient=VERTICAL)  
         self.document_table = ttk.Treeview(table2, column=("Nama", "NIM", "Prodi", "File"), xscrollcommand=scroll_x.set)
@@ -76,21 +122,22 @@ class document():
         scroll_x.config(command=self.document_table.xview)
         #=================================
 
-        #Tabel 3   
-        table3 = Frame(self.root, bd=4, relief=RIDGE, bg="green")
+        #TABEL 3  
+        table3 = Frame(self, bd=4, relief=RIDGE, bg="#D9D9D9")
         table3.place(y=410, x=10, width=268, height=55)
 
-        #add, clear, edit, save
-        addbtn = ttk.Button(table3, text="Tambah", command=self.add_document).grid(row=0,column=0, padx=5, pady=10)
-        editbtn = ttk.Button(table3, text="Edit", command=self.edit_data).grid(row=0,column=1, padx=5, pady=10) 
-        clearbtn = ttk.Button(table3, text="Bersihkan", command=self.clear).grid(row=0,column=3, padx=5, pady=10)
+        #ADD, CLEAR, EDIT, SAVE
+        self.filepath="" #Mempertahankan File ketika melakukan Edit
+        addbtn = Button(table3, text="Tambah", command=self.add_document, width="9", bg="#A1A5A6").grid(row=0,column=0, padx=7, pady=10)
+        editbtn = Button(table3, text="Edit", command=self.edit_data, width="9", bg="#A1A5A6").grid(row=0,column=1, padx=7, pady=10) 
+        clearbtn = Button(table3, text="Bersihkan", command=self.clear, width="9", bg="#A1A5A6").grid(row=0,column=3, padx=7, pady=10)
 
-        #Add File
-        self.addfilebtn = Button(table1, text="Add File", command=self.openfile)
+        #MENU TAMBAH FILE
+        self.addfilebtn = Button(table1, text="Add File", command=self.openfile, width="9", bg="#A1A5A6")
         self.addfilebtn.place(x=110,y=260)
         #====================================
 
-        #Document Table
+        #ISI TABEL
         self.document_table.heading("Nama", text="Nama")
         self.document_table.heading("NIM", text="NIM")
         self.document_table.heading("Prodi", text="Prodi")
@@ -105,39 +152,33 @@ class document():
         self.show_data()
 
 
-    #Program
-    def openfile(self, event=None):
-        self.filepath = filedialog.askopenfilename(initialdir="D:/", filetypes=(("all files", "*.*"),("png files", "*.png"),
-                                                                            ("jpg files", "*.jpg"), ("pdf files", "*.pdf"),
-                                                                            ("docx files","*docx")))
-        self.label = Label(text = "")
-        self.label.grid(padx =10, pady=350)
-        self.label.configure(text = self.filepath, bg="yellow")
-        print('Selected:',self.filepath)
-    
 
+class proses_data():
+    #PROGRAM PROSES DATA
     def convertToBinaryData(self, filename):
         # Convert digital data to binary format
         with open(filename, 'rb') as file:
             binaryData = file.read()
-        return binaryData
+            string_path = os.path.split(filename)
+        return binaryData, string_path[1]
 
 
-    def insertBLOB(self, nama, nim, prodi, digitalFile):
+    def insertBLOB(self, nama, nim, prodi, digitalFile=None):
         print("Inserting BLOB into document table")
-        con=pymysql.connect(host="localhost", user="root", password="", database="document")
-        cur=con.cursor()
+        con, cur = self.connect_start()
         sql_insert_blob_query = """ INSERT INTO document
-        (nama, nim, prodi, file) VALUES (%s,%s,%s,%s)"""
-        binaryFile = self.convertToBinaryData(digitalFile)
+        (nama, nim, prodi, filename, file) VALUES (%s,%s,%s,%s,%s)"""
+        print(digitalFile)
+        if digitalFile != None :
+            binaryFile, string_path = self.convertToBinaryData(digitalFile)
+        else :
+            binaryFile=""
+            string_path=""
 
-        # Convert data into tuple format
-        insert_blob_tuple = (nama, nim, prodi, binaryFile)
+        insert_blob_tuple = (nama, nim, prodi, string_path, binaryFile)
         result = cur.execute(sql_insert_blob_query, insert_blob_tuple)
-        con.commit()
         print("File inserted successfully as a BLOB into document table", result)
-        cur.close()
-        con.close()
+        self.connect_end(con,cur)
         print("MySQL connection is closed")
 
 
@@ -149,8 +190,7 @@ class document():
 
     def readBLOB(self, nim, digitalFile):
         print("Reading BLOB data from document table")
-        con=pymysql.connect(host="localhost", user="root", password="", database="document")
-        cur=con.cursor()
+        con, cur = self.connect_start()
         sql_fetch_blob_query = """SELECT * from document where nim = %s"""
         cur.execute(sql_fetch_blob_query, (nim,))
         record = cur.fetchall()
@@ -158,51 +198,115 @@ class document():
             print("nama = ", row[0], )
             print("nim = ", row[1], )
             print("prodi = ", row[2])
-            file = row[3]
+            file = row[4]
             print("Storing file mahasiswa on disk \n")
             self.write_file(file, digitalFile)
-        cur.close()
-        con.close()
+        self.connect_end(con,cur)
         print("MySQL connection is closed")
 
 
-    def add_document(self):
-        self.insertBLOB (self.var_name.get(),self.var_nim.get(),self.var_prodi.get(),self.filepath)
-        
-        self.show_data()
-        self.clear()
+    def prosesedit(self, nama, nim, prodi, digitalFile):
+        con, cur = self.connect_start()
+        if (digitalFile == ""):
+            sql_check_query = """SELECT * from document where nim = %s"""
+            cur.execute(sql_check_query, (nim,))
+            record = cur.fetchall()
+            for row in record:
+                check_nama = row[0]
+                check_prodi = row[2]
+            if (check_nama == nama and check_prodi == prodi):
+                print("No Updated")
+            else :
+                sql_update_query = """UPDATE document set nama = %s, prodi = %s where nim = %s"""
+                input_data = (nama, prodi, nim)
+                cur.execute(sql_update_query, input_data)
+                print("Record NIM =",(nim),"Updated successfully")
 
-    def download(self):
-        self.filesave = filedialog.asksaveasfilename(initialdir="D:/", filetypes=(("all files", "*.*"),("png files", "*.png"),
+        else :
+            sql_update_query = """UPDATE document set nama = %s, prodi = %s, filename = %s, file = %s where nim = %s"""
+            binaryFile, string_path = self.convertToBinaryData(digitalFile)
+            input_data = (nama, prodi, string_path, binaryFile, nim)
+            cur.execute(sql_update_query, input_data)
+            print("Record NIM =",(nim),"Updated successfully")
+        self.connect_end(con,cur)
+    #--------------------------------------------------
+
+
+
+class program(document, proses_data):
+    def __init__(self, root, controller):
+        super().__init__(root, controller)
+
+    #PROGRAM AKSES DATA
+    def openfile(self):
+        self.filepath = filedialog.askopenfilename(initialdir="D:/Kuliah", filetypes=(("all files", "*.*"),("png files", "*.png"),
                                                                             ("jpg files", "*.jpg"), ("pdf files", "*.pdf"),
                                                                             ("docx files","*docx")))
+        if (self.filepath!=""):
+            self.label.configure(text = self.filepath, bg="yellow")
+            print('Selected:',self.filepath)
+    
+
+    def add_document(self):
+        self.insertBLOB (self.var_name.get(),self.var_nim.get(),self.var_prodi.get(),self.filepath)
+
+
+    def download(self):
+        con, cur = self.connect_start()
+        sql_check_query = """SELECT filename from document where nim = %s"""
+        cur.execute(sql_check_query, (self.var_nim.get()))
+        record = cur.fetchall()
+        for row in record :
+            filename = row[0]
+        con.commit()
+        cur.close()
+        con.close()
+        self.filesave = filedialog.asksaveasfilename(initialdir="D:/", initialfile=filename, 
+                                                    filetypes=(("all files", "*.*"),("png files", "*.png"),
+                                                                ("jpg files", "*.jpg"), ("pdf files", "*.pdf"),
+                                                                ("docx files","*docx")))
         myfile=open(self.filesave, "w+")
         self.readBLOB (self.var_nim.get(), self.filesave)
 
-    def show_data(self):
-        con=pymysql.connect(host="localhost", user="root", password="", database="document")
-        cur=con.cursor()
-        cur.execute("select * from document")
-        rows=cur.fetchall()
-        if len(rows)!=0:
-            self.document_table.delete(*self.document_table.get_children())
-            for row in rows:
-                self.document_table.insert("", END, values=row)
-            con.commit()
-        con.close()
+
+    def edit_data(self):
+        self.prosesedit (self.var_name.get(),self.var_nim.get(),self.var_prodi.get(),self.filepath)
+
+
+    def delete_data(self):
+        con, cur = self.connect_start()
+        cur.execute("DELETE from document where nim=%s", self.var_nim.get())
+        self.connect_end(con,cur)
     
 
     def search_data(self):
-        con=pymysql.connect(host="localhost", user="root", password="", database="document")
-        cur=con.cursor()
-        cur.execute("select * from document where nama LIKE '%"+str(self.var_search.get())+"%'" 
+        con, cur = self.connect_start()
+        cur.execute("SELECT * from document where nama LIKE '%"+str(self.var_search.get())+"%'" 
                                                     "or nim LIKE '%"+str(self.var_search.get())+"%'"
                                                     "or prodi LIKE '%"+str(self.var_search.get())+"%'")
         rows=cur.fetchall()
         if len(rows)!=0:
             self.document_table.delete(*self.document_table.get_children())
             for row in rows:
-                 self.document_table.insert("", END, values=row)
+                data = list(row)
+                if (data[3]==""):
+                    data[3]= str("File Tidak Tersedia")
+                self.document_table.insert("", END, values=data)
+            con.commit()
+        con.close()
+
+
+    def show_data(self):
+        con, cur = self.connect_start()
+        cur.execute("SELECT * from document")
+        rows=cur.fetchall()
+        if len(rows)!=0:
+            self.document_table.delete(*self.document_table.get_children())
+            for row in rows:
+                data = list(row)
+                if (data[3]==""):
+                    data[3]= str("File Tidak Tersedia")
+                self.document_table.insert("", END, values=data)
             con.commit()
         con.close()
 
@@ -211,49 +315,46 @@ class document():
         self.var_name.set(""),
         self.var_nim.set(""),
         self.var_prodi.set(""),
+        self.label.configure(text = "", bg="#D9D9D9")
+        self.entry_nim.config(state="normal")
+        self.filepath=""
+
 
     def get_cursor(self, ev):
         cursor_row=self.document_table.focus()
         content=self.document_table.item(cursor_row)
         row=content["values"]
         self.var_name.set(row[0])
+        self.entry_nim.config(state="disabled")
         self.var_nim.set(row[1])
         self.var_prodi.set(row[2])
+        
 
-
-    def edit_data(self):
+    def connect_start(self):
         con=pymysql.connect(host="localhost", user="root", password="", database="document")
         cur=con.cursor()
-        cur.execute("update document set nama=%s, prodi=%s, file=%s where nim=%s",(self.var_name.get(),
-                                                                                  self.var_prodi.get(),
-                                                                                  self.openfile,
-                                                                                  self.var_nim.get()
-                                                                                  ))
-        con.commit()
-        self.show_data()
-        self.clear()
-        con.close()
+        return con, cur
 
 
-    def delete_data(self):
-        con=pymysql.connect(host="localhost", user="root", password="", database="document")
-        cur=con.cursor()
-        cur.execute("delete from document where nim=%s", self.var_nim.get())
+    def connect_end(self, con,cur):
         con.commit()
+        cur.close()
         con.close()
         self.show_data()
         self.clear()
 
 
-     #Menu Keluar
-    def close(self, event=None):
-        if mb.askyesno('Konfirmasi', 'Keluar dari program?', parent=self.root):
-            self.root.destroy()
+    #MENU KELUAR
+    #def close(self, event=None):
+        #if mb.askyesno('Konfirmasi', 'Keluar dari program?', parent=self.root):
+            #self.root.destroy()
     #====================================    
 
-#Output
+
+#OUTPUT
 if __name__ == '__main__':
-    root = Tk()
-    root.configure(background="#3cc7f7")
-    apk = document(root)
-    root.mainloop()
+    app = MyApp()
+    app.title('Document Managment System')
+    app.geometry("1020x540")
+    app.resizable(False, False)
+    app.mainloop()
